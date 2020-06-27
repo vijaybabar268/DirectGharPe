80c7d10f1eb4f0b835ef7b60dadf322ed004fd46
vijay babar <vijbab.kalp@gmail.com>
2020-06-27 23:12:35 +0530

    Created Admin Area MVC 5.

---
+++ DirectGharPe/DirectGharPe/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DirectGharPe.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            return Content("Hello from Product controller admin panel.");
+        }
+    }
+}
