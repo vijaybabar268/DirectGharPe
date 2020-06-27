61618a0b4007e56c5bd426b0e01ad986df1fef98
vijay babar <vijbab.kalp@gmail.com>
2020-06-27 12:39:36 +0530

    Project Created.

---
+++ DirectGharPe/DirectGharPe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DirectGharPe.MvcApplication" Language="C#" %>
---
+++ DirectGharPe/DirectGharPe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DirectGharPe.Startup))]
+namespace DirectGharPe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
