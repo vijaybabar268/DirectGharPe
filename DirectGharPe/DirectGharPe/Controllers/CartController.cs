70a727ab45e94b6ab9034b1a191105214bb56500
vijay babar <vijbab.kalp@gmail.com>
2020-07-18 22:33:56 +0530

    Add to Cart Functionality Starting

---
+++ DirectGharPe/DirectGharPe/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DirectGharPe.Controllers
+{
+    public class CartController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(string id, string name, string price, string quantity, string action)
+        {
+            return Content("Added to Cart");
+        }
+
+        public ActionResult BuyNow()
+        {
+            return View();
+        }
+
+    }
+}
